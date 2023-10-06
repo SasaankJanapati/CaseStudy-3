60ec6a3e78294e9ff052776b5ca44bb94f171ad6
sonith17 <muppirivishnusonith@gmail.com>
2023-10-06 16:55:33 +0530

    commit

--- Customer.java
+++ Customer.java
@@ -8,4 +8,25 @@ public class Customer {
     private String userName;
     private String password;
     private ArrayList<Policy> policies;
+
+    Customer() {
+    }
+    /*
+     * Customer(String name, String phoneNumber, String email, String userName,
+     * String password) {
+     * this.name = name;
+     * this.setPhoneNumber(phoneNumber);
+     * this.email = email;
+     * this.userName = userName;
+     * this.password = password;
+     * }
+     */
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
