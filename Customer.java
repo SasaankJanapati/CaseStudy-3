e58dbf2a6f6f5625b9db0a56de5caa16a24dc1d5
Shiva936 <shivadharshansankar936@gmail.com>
2023-10-06 17:15:00 +0530

    Added Policy creation method

--- Customer.java
+++ Customer.java
@@ -8,4 +8,28 @@ public class Customer {
     private String userName;
     private String password;
     private ArrayList<Policy> policies;
+
+    Customer(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        this.name = sc.next();
+        System.out.println("Enter your name");
+        this.phoneNumber = sc.next();
+        System.out.println("Enter your name");
+        this.email = sc.next();
+        System.out.println("Enter your name");
+        this.address = sc.next();
+        System.out.println("Enter your name");
+        this.userName = sc.next();
+        System.out.println("Enter your name");
+        this.password = sc.next();
+    }
+
+    protected void addPolicy(Policy policy){
+        policies.add(policy);
+    }
+
+    public String getName() { //later change it to id
+        return name;
+    }
 }
--- Policy.java
+++ Policy.java
@@ -1,8 +1,25 @@
+import java.util.*;
+
 public class Policy {
     private boolean isActive;
     private String type;
-    private long id;
+    private String id;
     private double premiumAmount;
     private double coverage;
     private double period;
+
+    Policy(){
+        this.isActive = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the type");
+        this.type = sc.next();
+        System.out.println("Enter the type");
+        this.id = UUID.randomUUID().toString().substring(0, 7);
+        System.out.println("Enter the type");
+        this.premiumAmount = sc.nextDouble();
+        System.out.println("Enter the type");
+        this.coverage = sc.nextDouble();
+        System.out.println("Enter the type");
+        this.period = sc.nextDouble();
+    }
 }
