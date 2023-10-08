33336bb767bd643bc43f20e53b3646549556dac9
sonith17 <muppirivishnusonith@gmail.com>
2023-10-08 09:49:42 +0530

    print statement update

--- Policy.java
+++ Policy.java
@@ -8,18 +8,18 @@ public class Policy {
     private double coverage;
     private double period;
 
-    Policy(){
+    Policy() {
         this.isActive = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the type");
         this.type = sc.next();
-        System.out.println("Enter the type");
+        System.out.println("Enter the id");
         this.id = UUID.randomUUID().toString().substring(0, 7);
-        System.out.println("Enter the type");
+        System.out.println("Enter the premiun amount");
         this.premiumAmount = sc.nextDouble();
-        System.out.println("Enter the type");
+        System.out.println("Enter the coverage");
         this.coverage = sc.nextDouble();
-        System.out.println("Enter the type");
+        System.out.println("Enter the period");
         this.period = sc.nextDouble();
     }
 
