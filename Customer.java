53b11accfff5d3b4d67512f7504c9ec9f3ce27ca
sonith17 <muppirivishnusonith@gmail.com>
2023-10-09 18:06:10 +0530

    If commits empty

--- Customer.java
+++ Customer.java
@@ -1,15 +1,16 @@
 import java.util.*;
 
 public class Customer {
-    
-    static Scanner sc=new Scanner(System.in);
+
+    static Scanner sc = new Scanner(System.in);
     private String name;
-    private String phoneNumber; 
+    private String phoneNumber;
     private String email;
     private String address;
     private String userName;
     private String password;
     private boolean isBlocked = false;
+
     public boolean isBlocked() {
         return isBlocked;
     }
@@ -17,15 +18,17 @@ public class Customer {
     public void setBlocked(boolean isBlocked) {
         this.isBlocked = isBlocked;
     }
+
     private ArrayList<Policy> policies;
     private ArrayList<Claim> claims;
-    Customer(String name, String phoneNumber, String email,String address, String userName, String password) {
-        this.policies=new ArrayList<Policy>();
+
+    Customer(String name, String phoneNumber, String email, String address, String userName, String password) {
+        this.policies = new ArrayList<Policy>();
         this.claims = new ArrayList<Claim>();
         this.setName(name);
         this.setPhoneNumber(phoneNumber);
         this.email = email;
-        this.address=address;
+        this.address = address;
         this.setUserName(userName);
         this.setPassword(password);
     }
@@ -58,8 +61,8 @@ public class Customer {
         this.userName = userName;
     }
 
-    Customer(){
-        this.policies=new ArrayList<Policy>();
+    Customer() {
+        this.policies = new ArrayList<Policy>();
         this.claims = new ArrayList<Claim>();
         System.out.println("Enter the Customer name");
         this.name = sc.next();
@@ -74,6 +77,7 @@ public class Customer {
         System.out.println("Enter the Customer password");
         this.password = sc.next();
     }
+
     protected void addPolicy(Policy policy) {
         this.policies.add(policy);
     }
@@ -82,8 +86,8 @@ public class Customer {
         claims.add(claim);
     }
 
-    protected Claim createNewClaim(Customer Cu,Database database)  throws InterruptedException{
-        displayPoliciesId(Cu,database);
+    protected Claim createNewClaim(Customer Cu, Database database) throws InterruptedException {
+        displayPoliciesId(Cu, database);
         System.out.print("\n\t\tPlease Enter your policy ID : ");
         String policyId = sc.nextLine();
         for (Policy policy : policies) {
@@ -96,68 +100,74 @@ public class Customer {
         }
         return null;
     }
-    protected void displayPoliciesId(Customer Cu,Database database) throws InterruptedException{
-        InsuranceManagement Management=new InsuranceManagement();
+
+    protected void displayPoliciesId(Customer Cu, Database database) throws InterruptedException {
+        InsuranceManagement Management = new InsuranceManagement();
         System.out.print("\n\t\tYour policies : ");
         for (Policy policy : policies) {
-            System.out.println("\t\t"+policy.getId());
+            System.out.println("\t\t" + policy.getId());
         }
         return;
     }
-    protected void displayPolicies(Customer Cu,Database database) throws InterruptedException{
-        InsuranceManagement Management=new InsuranceManagement();
+
+    protected void displayPolicies(Customer Cu, Database database) throws InterruptedException {
+        InsuranceManagement Management = new InsuranceManagement();
         System.out.print("\n\t\tYour policies : ");
         for (Policy policy : policies) {
-            System.out.println("\t\t"+policy.getId());
+            System.out.println("\t\t" + policy.getId());
         }
         System.out.println("Do you want to see the details of your policies\tpress 1\nTo go to Main Menu\t\tpress 2");
-        int t=sc.nextInt();
-        if(t==1){
+        int t = sc.nextInt();
+        if (t == 1) {
             boolean isValid = false;
-            while(!isValid){
+            while (!isValid) {
                 System.out.println("Choose a Policy Id");
                 Scanner sc = new Scanner(System.in);
                 String policyId = sc.next();
                 for (Policy policy : policies) {
-                    if(policy.getId().compareTo(policyId) == 0){
+                    if (policy.getId().compareTo(policyId) == 0) {
                         policy.displayPolicy();
                         isValid = true;
                     }
                 }
-                if(!isValid){
+                if (!isValid) {
                     System.out.println("Invalid Policy Id");
                     System.out.println("");
                 }
             }
             System.out.print("\t\tPress 1 to go to main menu :");
-            int x=sc.nextInt();
-            InsuranceManagement.customerPortalDisplay(Cu,database);
+            int x = sc.nextInt();
+            InsuranceManagement.customerPortalDisplay(Cu, database);
             return;
-        }else{
-            InsuranceManagement.customerPortalDisplay(Cu,database);
+        } else {
+            InsuranceManagement.customerPortalDisplay(Cu, database);
             return;
         }
     }
-    protected void displayClaims(){
+
+    protected void displayClaims() {
         System.out.println("Your claims");
         for (Claim claim : claims) {
             System.out.println(claim.getId());
         }
+        if (claims.size() == 0) {
+            System.out.println("You have 0 Claims");
+        }
         boolean isValid = false;
-        while(!isValid){
+        while (!isValid) {
             System.out.println("Choose a Claim Id");
             Scanner sc = new Scanner(System.in);
             String claimId = sc.next();
             for (Claim claim : claims) {
-                if(claim.getId().compareTo(claimId) == 0){
+                if (claim.getId().compareTo(claimId) == 0) {
                     claim.displayClaim();
                     isValid = true;
                 }
             }
-            if(!isValid){
+            if (!isValid) {
                 System.out.println("Invalid Claim Id");
             }
         }
     }
-       
+
 }
