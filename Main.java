26958340a13914ac767553233a28433302fdfb56
sonith17 <muppirivishnusonith@gmail.com>
2023-10-09 22:10:41 +0530

    with added comments and camelCasing

--- Main.java
+++ Main.java
@@ -4,38 +4,38 @@ public class Main {
 
   static Scanner sc = new Scanner(System.in);
 
-    // Predefined Users for Test case
-    static void preDefined(Database Db) {
-        Customer c1 = new Customer("Akilesh", "1234567899", "ak@gmaill.com","India", "ak11", "akilesh");
-        Customer c2 = new Customer("Mokshith", "1230567899", "mo@gmaill.com","India", "mo11", "mokshith");
-        Customer c3 = new Customer("Sonith", "1234547899", "so@gmaill.com","India", "so11", "sonith");
-        Customer c4 = new Customer("Shivadarshan", "2234567899", "sh@gmaill.com","India", "sh11", "shivadarshan");
-        Customer c5 = new Customer("Sasaank", "1234767899", "sa@gmaill.com","India" ,"sa11", "sasaank");
-        Db.addCustomer(c1);
-        Db.addCustomer(c2);
-        Db.addCustomer(c3);
-        Db.addCustomer(c4);
-        Db.addCustomer(c5);
-       Agent agent=new Agent("A", "123", "agent", "agent", Db);
-       Db.addAgent(agent);
-       agent.createNewPolicy(c1,Db);
-       agent.createNewPolicy(c2,Db);
-       agent.createNewPolicy(c3,Db);
-       agent.createNewPolicy(c4,Db);
-       agent.createNewPolicy(c5,Db);
-       Adjuster adjuster = new Adjuster("Ram","11223344","ra11","ram",Db);
-       Db.addAdjuster(adjuster);
-     }
+  // Predefined Users for Test case, we used static keyword just to add test data
+  // not used any more than that!
+  static void preDefined(Database database) {
+    Customer customer1 = new Customer("Akilesh", "1234567899", "ak@gmaill.com", "India", "ak11", "akilesh");
+    Customer customer2 = new Customer("Mokshith", "1230567899", "mo@gmaill.com", "India", "mo11", "mokshith");
+    Customer customer3 = new Customer("Sonith", "1234547899", "so@gmaill.com", "India", "so11", "sonith");
+    Customer customer4 = new Customer("Shivadarshan", "2234567899", "sh@gmaill.com", "India", "sh11", "shivadarshan");
+    Customer customer5 = new Customer("Sasaank", "1234767899", "sa@gmaill.com", "India", "sa11", "sasaank");
+    database.addCustomer(customer1);
+    database.addCustomer(customer2);
+    database.addCustomer(customer3);
+    database.addCustomer(customer4);
+    database.addCustomer(customer5);
+    Agent agent = new Agent("A", "123", "agent", "agent", database);
+    database.addAgent(agent);
+    agent.createNewPolicy(customer1, database);
+    agent.createNewPolicy(customer2, database);
+    agent.createNewPolicy(customer3, database);
+    agent.createNewPolicy(customer4, database);
+    agent.createNewPolicy(customer5, database);
+    Adjuster adjuster = new Adjuster("Ram", "11223344", "ra11", "ram", database);
+    database.addAdjuster(adjuster);
+  }
 
+  public static void main(String[] args) throws InterruptedException {
+    Database database = new Database();
+    ArrayList<Customer> customer = new ArrayList<Customer>();
+    ArrayList<Agent> agent = new ArrayList<Agent>();
+    ArrayList<Adjuster> adjuster = new ArrayList<Adjuster>();
 
-    public static void main(String[] args) throws InterruptedException {
-        Database Db=new Database();
-        ArrayList<Customer> Cu = new ArrayList<Customer>();
-        ArrayList<Agent> Ag = new ArrayList<Agent>();
-        ArrayList<Adjuster> Ad = new ArrayList<Adjuster>();
-        
-        preDefined(Db);
-        InsuranceManagement Im=new InsuranceManagement();
-        Im.mainFunction(Db);
-    }
+    preDefined(database);
+    InsuranceManagement insuranceManagement = new InsuranceManagement();
+    insuranceManagement.mainFunction(database);
+  }
 }
