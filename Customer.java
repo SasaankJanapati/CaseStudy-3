4e06c55642c0ea80241d6d0b150127aa3b8cc6e2
sonith17 <muppirivishnusonith@gmail.com>
2023-10-09 21:55:31 +0530

    Customer class with comments and camelCases

--- Customer.java
+++ Customer.java
@@ -12,17 +12,18 @@ public class Customer {
     private String password;
     private boolean isBlocked = false;
 
-    public boolean isBlocked() {
+    public boolean isBlocked() {// if customer types wrong password more number he gets blocked
         return isBlocked;
     }
 
-    public void setBlocked(boolean isBlocked) {
+    public void setBlocked(boolean isBlocked) {// setter for isBlocked
         this.isBlocked = isBlocked;
     }
 
-    private ArrayList<Policy> policies;
+    private ArrayList<Policy> policies;// arrayList to contain policies and claims of customer
     private ArrayList<Claim> claims;
 
+    // parametrised constructor for customer
     Customer(String name, String phoneNumber, String email, String address, String userName, String password) {
         this.policies = new ArrayList<Policy>();
         this.claims = new ArrayList<Claim>();
@@ -34,6 +35,7 @@ public class Customer {
         this.setPassword(password);
     }
 
+    // getter and setter of name
     public String getName() {
         return name;
     }
@@ -42,6 +44,7 @@ public class Customer {
         this.name = name;
     }
 
+    // getter and setter for password
     public String getPassword() {
         return password;
     }
@@ -50,10 +53,12 @@ public class Customer {
         this.password = password;
     }
 
+    // setter for phonenumber
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    // getter and setter for username
     public String getUserName() {
         return userName;
     }
@@ -62,6 +67,7 @@ public class Customer {
         this.userName = userName;
     }
 
+    // constructor for customer (it is used when agent adds policy for new customer)
     Customer() {
         this.policies = new ArrayList<Policy>();
         this.claims = new ArrayList<Claim>();
@@ -79,26 +85,32 @@ public class Customer {
         this.password = sc.next();
     }
 
+    // adds newly created policies of customer to policies arraylist
     protected void addPolicy(Policy policy) {
         this.policies.add(policy);
     }
 
+    // adds newly created claims of customer to claims arraylist
     protected void addClaim(Claim claim) {
         claims.add(claim);
     }
 
-    protected Claim createNewClaim(Customer Cu, Database database) throws InterruptedException {
-        Scanner sc=new Scanner(System.in);
-        displayPoliciesId(Cu, database);
+    // below method enables customer to create new claims and add them to database
+    // and claims by customer
+    protected Claim createNewClaim(Customer customer, Database database) throws InterruptedException {// exception to
+                                                                                                      // delay time
+        Scanner sc = new Scanner(System.in);
+        displayPoliciesId(customer, database);
         System.out.print("\n\t\tPlease Enter your policy ID : ");
         String policyId = sc.nextLine();
-
+        // takes policy id to which he wanted to claim and checks with policies he have
+        // and gives corresponding outputs returns the claim created
         for (Policy policy : policies) {
             if (policy.getId().compareTo(policyId) != 0) {
                 System.out.println("\n\t\t! Policy id not found\n\t\tPress 1 to go to Main Menu");
-                int t=sc.nextInt();
-                if (t==1) {
-                    InsuranceManagement.customerPortalDisplay(Cu, database);
+                int t = sc.nextInt();
+                if (t == 1) {
+                    InsuranceManagement.customerPortalDisplay(customer, database);
                     return null;
                 }
             } else {
@@ -109,7 +121,9 @@ public class Customer {
         return null;
     }
 
-    protected void displayPoliciesId(Customer Cu, Database database) throws InterruptedException {
+    // displays the policiesId's which customer have so there is no use print all
+    // policies clearly
+    protected void displayPoliciesId(Customer customer, Database database) throws InterruptedException {
         InsuranceManagement Management = new InsuranceManagement();
         System.out.print("\n\t\tYour policies : ");
         for (Policy policy : policies) {
@@ -118,8 +132,10 @@ public class Customer {
         return;
     }
 
-    protected void displayPolicies(Customer Cu, Database database) throws InterruptedException {
-        
+    // prints policies id and prints the complete details of choosen policy if it
+    // exists(customerPortalDisplay takes us back to customer page)
+    protected void displayPolicies(Customer customer, Database database) throws InterruptedException {
+
         System.out.print("\n\t\tYour policies : ");
         for (Policy policy : policies) {
             System.out.println("\t\t" + policy.getId());
@@ -145,23 +161,25 @@ public class Customer {
             }
             System.out.print("\t\tPress 1 to go to main menu :");
             int x = sc.nextInt();
-            InsuranceManagement.customerPortalDisplay(Cu, database);
+            InsuranceManagement.customerPortalDisplay(customer, database);
             return;
         } else {
-            InsuranceManagement.customerPortalDisplay(Cu, database);
+            InsuranceManagement.customerPortalDisplay(customer, database);
             return;
         }
     }
 
-    protected void displayClaims(Customer cu,Database database)throws InterruptedException {
+    // prints claims id and prints the complete details of choosen claims if it
+    // exists(customerPortalDisplay takes us back to customer page)
+    protected void displayClaims(Customer customer, Database database) throws InterruptedException {
         System.out.println("Your claims");
         for (Claim claim : claims) {
             System.out.println(claim.getId());
         }
         if (claims.size() == 0) {
             System.out.println("You have 0 Claims");
-           Management.customerPortalDisplay(cu,database);
-           return;
+            Management.customerPortalDisplay(customer, database);
+            return;
         }
         boolean isValid = false;
         while (!isValid) {
